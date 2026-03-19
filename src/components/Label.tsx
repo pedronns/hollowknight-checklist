d8817c84f99ff33a576bb416d4928c9b2eef9a32
Pedro <pedro.dnunes1@gmail.com>
2026-03-19 16:22:43 -0300

    align flourish images to end

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -4,12 +4,12 @@ type Props = {
 
 const Label = ({ title }: Props) => {
   return (
-    <div className="flex gap-1 items-center justify-center max-w-full break-words">
-      <img src="/img/flourish.png" className="scale-x-[-1] h-6" />
+    <div className="flex gap-1 items-end justify-center max-w-full break-words">
+      <img src="/img/flourish.png" className="scale-x-[-1] h-6 mb-0.5" />
       <p className="flex-1 text-2xl capitalize font-semibold hover:underline break-words text-center">
         {title}
       </p>
-      <img src="/img/flourish.png" className="h-6" />
+      <img src="/img/flourish.png" className="h-6 mb-0.5" />
     </div>
   )
 }
