e2c21294b6939bc5e6d399b8572c0d5cf3b407ff
Pedro <pedro.dnunes1@gmail.com>
2026-03-22 20:48:50 -0300

    Add custom font

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -6,7 +6,7 @@ const Label = ({ title }: Props) => {
   return (
     <div className="flex gap-1 items-end justify-center max-w-full break-words">
       <img src="/img/flourish.png" className="scale-x-[-1] h-6 mb-0.5" />
-      <p className="flex-1 text-2xl capitalize font-semibold hover:underline break-words text-center">
+      <p className="flex-1 text-2xl capitalize font-bold hover:underline break-words text-center">
         {title}
       </p>
       <img src="/img/flourish.png" className="h-6 mb-0.5" />
